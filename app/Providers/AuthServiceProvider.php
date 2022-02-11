1b1e7dd99299ccfcf8a9eb73c135944a7d0ef76f
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2022-02-11 23:28:44 +0330

    add perm and role authorize functionality

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,36 +27,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        $perms = Permission::all();
+        // grant all abilities to admin role
 
-        foreach ($perms as $perm) {
-            Gate::define($perm->title, function ($user) use ($perm) {
-                $user_perms = $this->fetch_user_perms();
-                return $user_perms->contains('title', $perm->title);
-            });
-        }
-    }
+        Gate::before(function ($user, $ability) {
+            if ($user->roles->contains('name', 'admin')) {
+                return true;
+            }
+        });
 
-    private function fetch_user_perms()
-    {
-        // if user was not authenticated , add quest permissions to him
-
-        if (auth()->check()) {
-            $roles = auth()->user()->roles;
-        } else {
-            $roles = Role::firstOrCreate([
-                'name' => 'guest'
-            ])->get();
-        }
-
-        // create empty collection and store all perms of all user roles by merging them
-
-        $perms = collect();
-
-        foreach ($roles as $role) {
-            $perms = $perms->merge($role->permissions);
-        }
-
-        return $perms;
+        Gate::define('viewAdminPanel', function ($user) {
+            return false;
+        });
     }
 }
