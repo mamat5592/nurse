c700c7d4c5f15b318ffa5ac4b010aeb60d06ce7c
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2022-02-12 14:16:50 +0330

    refactor permission policy

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -55,40 +55,4 @@ class PermissionPolicy
     {
         return false;
     }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Permission $permission)
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Permission $permission)
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Permission  $permission
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Permission $permission)
-    {
-        return false;
-    }
 }
